97e8f69ba219950328d4667ba765003f1667e1d1
hrsma2i <hrs.ma2i@gmail.com>
2018-09-23 19:17:39 +0900

    Add initialization and remove images

--- dl_imgs.py
+++ dl_imgs.py
@@ -8,18 +8,32 @@ from InstagramAPI import InstagramAPI
 
 
 def get_url(media):
-    return media['image_versions2']\
-        ['candidates'][0]['url']
-        
+    if 'video_versions' in media.keys():
+        return media['video_versions'][0]['url']
+    else:
+        return media['image_versions2']\
+            ['candidates'][0]['url']
+
 
 def download_img():
     secrete_file = './secrete.txt'
 
+    if not os.path.exists(secrete_file):
+        with open(secrete_file, 'w') as f:
+            pass
     with open(secrete_file) as f:
         username, password = [l.rstrip() for l in f.readlines()]
 
-    with open('./complete') as f:
+    complete_file = './complete.txt'
+    if not os.path.exists(complete_file):
+        with open(complete_file, 'w') as f:
+            pass
+    with open(complete_file) as f:
         completes = [l.rstrip() for l in f.readlines()]
+
+    out_dir = './downloads'
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
     api = InstagramAPI(username, password)
     api.login()
@@ -45,25 +59,26 @@ def download_img():
         for url in img_urls:
             res = requests.get(url)
             img_bin = res.content
-            out_dir = './downloads'
             out_name = os.path.basename(url)
-            out_name = out_name.split('jpg')[0] + 'jpg'
+            out_name = out_name.split('?')[0]
             print(out_name)
             out_file = os.path.join(out_dir, out_name)
             with open(out_file, 'wb') as f:
                 f.write(img_bin)
 
-    with open('./complete', 'a') as f:
+    with open(complete_file, 'a') as f:
         f.write('\n'.join([item['media']['id'] for item in items]))
         f.write('\n')
 
 
 def save_to_photo_app():
-   img_dir = './downloads' 
-   img_files = glob(os.path.join(img_dir, '*'))
+    img_dir = './downloads' 
+    img_files = glob(os.path.join(img_dir, '*'))
 
-   for img_file in img_files:
-        photos.create_image_asset(img_file) 
+    for img_file in img_files:
+        if img_file.split('.')[-1] == 'jpg':
+            photos.create_image_asset(img_file) 
+            os.remove(img_file)
 
 
 def main():
